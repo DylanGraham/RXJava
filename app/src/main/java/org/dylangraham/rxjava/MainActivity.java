952f16dd51c8376c89918e8fac7be6415b44fd3a
Dylan Graham <dylan.graham@gmail.com>
2016-11-07 16:40:11 +1100

    composite subscriptions!

--- app/src/main/java/org/dylangraham/rxjava/MainActivity.java
+++ app/src/main/java/org/dylangraham/rxjava/MainActivity.java
@@ -13,12 +13,13 @@ import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     private StuffGenerator stuffGenerator;
-    private Subscription stuffSubscription;
+    private CompositeSubscription allSubscriptions = new CompositeSubscription();
     @BindView(R.id.stuff_text) TextView stuffText;
 
     @Override
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                         stuffGenerator.generateStuff()
                 );
 
-        stuffSubscription = stuffObservable
+        Subscription subscription = stuffObservable
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
@@ -50,12 +51,16 @@ public class MainActivity extends AppCompatActivity {
                         (Throwable t) -> Timber.d(t.getMessage()),
                         this::onCompleted
                 );
+
+        allSubscriptions.add(subscription);
     }
 
     private void createNumbersObservable() {
-        stuffSubscription = Observable.interval(3, TimeUnit.SECONDS)
+        Subscription subscription = Observable.interval(3, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe((Long i) -> displayStuff(i.toString()));
+
+        allSubscriptions.add(subscription);
     }
 
     @OnClick(R.id.floating_action_button)
@@ -75,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (stuffSubscription != null && !stuffSubscription.isUnsubscribed()) {
-            stuffSubscription.unsubscribe();
+        if (allSubscriptions.hasSubscriptions() && !allSubscriptions.isUnsubscribed()) {
+            allSubscriptions.unsubscribe();
         }
     }
 }
