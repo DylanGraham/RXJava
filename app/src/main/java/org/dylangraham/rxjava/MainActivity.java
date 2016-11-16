0fbbb0849c6213fdd73909656caa18d25b954a1b
Dylan Graham <dylan.graham@gmail.com>
2016-11-16 13:44:44 +1100

    tidy up

--- app/src/main/java/org/dylangraham/rxjava/MainActivity.java
+++ app/src/main/java/org/dylangraham/rxjava/MainActivity.java
@@ -1,6 +1,7 @@
 package org.dylangraham.rxjava;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -12,53 +13,29 @@ import butterknife.OnClick;
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
-
-    private StuffGenerator stuffGenerator;
     private CompositeSubscription allSubscriptions = new CompositeSubscription();
-    @BindView(R.id.stuff_text) TextView stuffText;
+    @BindView(R.id.stuff_text)
+    TextView stuffText;
+    @BindView(R.id.floating_action_button)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (BuildConfig.DEBUG) Timber.plant(new Timber.DebugTree());
-        stuffGenerator = new StuffGenerator(this);
-        configureLayout();
-        //createObservable();
-        createNumbersObservable();
-    }
-
-    private void configureLayout() {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-    }
-
-    private void createObservable() {
-        Observable<String> stuffObservable =
-                Observable.fromCallable(() ->
-                        stuffGenerator.generateStuff()
-                );
-
-        Subscription subscription = stuffObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        this::displayStuff,
-                        (Throwable t) -> Timber.d(t.getMessage()),
-                        this::onCompleted
-                );
-
-        allSubscriptions.add(subscription);
+        createNumbersObservable();
     }
 
     private void createNumbersObservable() {
         Subscription subscription = Observable.interval(3, TimeUnit.SECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((Long i) -> displayStuff(i.toString()));
+                .subscribe(i -> displayStuff(i.toString()));
 
         allSubscriptions.add(subscription);
     }
